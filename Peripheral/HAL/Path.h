0730808e25730dfb962c4e14eecc2751ca8e7265
FireSoucery <scott@thefirebrandforge.com>
2022-05-31 13:44:03 -0700

    settings

--- Peripheral/HAL/Path.h
+++ Peripheral/HAL/Path.h
@@ -4,14 +4,14 @@
 #define XSTR(String) #String
 #define STR(String) XSTR(String)
 
+// HAL_Peripheral.h
+// CONFIG_HAL_PERIPHERAL_PATH
+// CONFIG_HAL_PERIPHERAL_PLATFORM
+// HAL_PERIPHERAL_PATH(File)
 
-// #if defined(CONFIG_PERIPHERAL_HAL_PATH_EXTERNAL)
-// #if defined(CONFIG_PERIPHERAL_HAL_PATH_INTERNAL_PLATFORM)
-
-
-#if defined(CONFIG_PATH_HAL_PERIPHERAL)
+#if 	defined(CONFIG_PATH_HAL_PERIPHERAL) 			/* External directory */
 	#define PATH_HAL_PERIPHERAL(File) STR(CONFIG_PATH_HAL_PERIPHERAL/File)
-#elif defined(CONFIG_PATH_HAL_PERIPHERAL_PLATFORM)
+#elif 	defined(CONFIG_PATH_HAL_PERIPHERAL_PLATFORM)	/* Library platform directory */
 	#define PATH_HAL_PERIPHERAL(File) STR(Peripheral/HAL/Platform/CONFIG_PATH_HAL_PERIPHERAL_PLATFORM/File)
 #endif
 
